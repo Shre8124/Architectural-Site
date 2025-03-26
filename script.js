577b7fe0cca5fd022e7753aafe40611f333f4e5d
Shreyash Saurabh <saurabhshreyash214@gmail.com>
2025-03-26 12:45:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Navigation scroll effect
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('scrolled', window.scrollY > 50);
+});
